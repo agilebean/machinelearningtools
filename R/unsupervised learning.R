93566227b3dc21a1193f7ab265c2c47f106a8caf
Chaehan So <chaehan.so@gmail.com>
2019-10-17 19:18:36 +0900

    added supervised learning.R. redesigned iterate_factor_analysis (moved from package::comfort)

---
+++ R/unsupervised learning.R
@@ -0,0 +1,45 @@
+################################################################################
+#
+# Topic:   Unsupervised Learning
+# Package: machinelearningtools
+# Purpose: Provide convenience functions for machine learning with caret
+#
+################################################################################
+iterate_factor_analysis <- function (items, max_factors = 5) {
+
+  require(dplyr)
+  require(purrr)
+  require(psych)
+
+  no_factors_labels <- 1:max_factors %>% as.character
+
+  result <- map_dfr(no_factors_labels, function(no_factors) {
+
+    pca.cor <- principal(items, nfactors = no_factors, covar = FALSE)
+    pca.cov <- principal(items, nfactors = no_factors, covar = TRUE)
+    pfa.cor <- fa(items, nfactors = no_factors, fm = "pa", cor = "cor")
+    pfa.cov <- fa(items, nfactors = no_factors, fm = "pa", cor = "cov")
+
+    list(pca.cor, pca.cov, pfa.cor, pfa.cov) %>%
+      set_names(c("pca.cor", "pca.cov", "pfa.cor", "pfa.cov")) %>%
+      map_df(function(EFA) {
+
+        cor.label <- EFA$Call %>% as.character() %>% .[length(.) ] %>% print
+        if(cor.label != "cor" & cor.label != "cov") {
+          cor.label <- ifelse(cor.label == FALSE, "cor", "cov")
+        }
+
+        tibble(
+          as.character(EFA$factors), EFA$fn, EFA$rms,
+          EFA$fit.off, cor.label, EFA$chi,
+          list(EFA), list(EFA$loadings), list(EFA$scores))
+
+      }) %>%
+      set_names(c("nfactors", "fn",  "RMSR", "fit.off",  "cor", "chi",
+                  "model", "loadings", "scores")) %>%
+      mutate_if(is.numeric, round, 2)
+
+  }) %T>% print
+
+  return(result)
+}
