96a1facedeac1acbf00bf9b6685a74c15350ccd8
Chaehan So <chaehan.so@gmail.com>
2019-11-30 17:57:18 +0900

    added perform_max_SMOTE in preprocessing.R

---
+++ R/preprocessing.R
@@ -0,0 +1,30 @@
+perform_max_SMOTE <- function(features, target, max_k) {
+
+  require(smotefamily)
+  for (k in max_k:1) {
+
+    training.set <- NULL
+    max_k_found <- FALSE
+
+    tryCatch({
+
+      training.set <- smotefamily::SMOTE(features, target, K = k) %>% .$data
+
+      # flag to break out of loop
+      if (!is.null(training.set)) max_k_found <- TRUE
+
+    },
+    error = function(e) {
+      cat("ERROR", conditionMessage(e), "\n")
+    },
+    warning = function(w) {
+      # cat("warning", conditionMessage(w), "\n")
+    }
+    )
+    if(max_k_found) {
+      cat("SMOTE: Found maximum k =", k, "\n")
+      break
+    }
+  }
+  return(training.set)
+}
