13b560d468f0088f81dc222fa09ae4790a63e2e6
Chaehan So <chaehan.so@gmail.com>
2019-12-24 20:52:50 +0900

    sentiment_freetext: added parameters normalize (default FALSE), lexicon

--- R/natural language processing.R
+++ R/natural language processing.R
@@ -21,17 +21,15 @@ unnest_freetext <- function(data, predictor, limit = NULL) {
 
 # get sentiment weights of a free-text field for one predictor label
 sentiment_freetext <- function(
-  data, predictor, limit = NULL, lexicon_label = "nrc"
+  data, text_column, limit = NULL, lexicon, normalize = FALSE
 ) {
   require(dplyr)
   require(tidyr)
   require(tidytext) # for stopwords, get_sentiments
 
-  nrc.dict <- get_sentiments(lexicon_label)
-
   unnested <- data %>%
-    select(!!predictor) %>%
-    unnest_tokens(word, !!predictor) %>%
+    select(!!text_column) %>%
+    unnest_tokens(word, !!text_column) %>%
     filter(!word %in% stop_words$word) %>%
     group_by(word) %>%
     summarise(count = n()) %>%
@@ -43,17 +41,23 @@ sentiment_freetext <- function(
   }
 
   # tricky: right_join (not inner_join) to keep all dict. sentiments
-  sentiment.ranking <- right_join(unnested, nrc.dict)
+  sentiment.ranking <- right_join(unnested, lexicon)
 
   # convert sentiment ranking (tibble) into predictors
   result <- sentiment.ranking %>%
     group_by(sentiment) %>%
     summarise(weight = sum(count, na.rm = TRUE)) %>%
-    # tricky: division by 0 would lead to NaN
-    # mutate(weight = weight/sum(weight)) %>%
-    mutate(weight = ifelse(weight == 0, 0, weight/sum(weight))) %>%
+    {
+      if (normalize) {
+        # tricky: division by 0 would lead to NaN
+        # mutate(weight = weight/sum(weight)) %>%
+        mutate(weight = ifelse(weight == 0, 0, weight/sum(weight)))
+      } else {
+        .
+      }
+    } %>%
     # tricky: spread all sentiment levels into columns
-    spread(sentiment, weight)
+    spread(sentiment, weight) %>% print
 
   return(result)
 }
