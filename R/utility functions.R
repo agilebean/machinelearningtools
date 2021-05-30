4bce64ece358d20b0a9e21a0639c4d77e31ff8cb
Chaehan So <chaehan.so@gmail.com>
2021-05-30 19:45:40 +0900

    renamed get_listelements_by_string > get_list_elements_by_string. Replaced str_detect by imap_lgl and avoided second call to input_list

--- R/utility functions.R
+++ R/utility functions.R
@@ -1,8 +1,9 @@
 
-get_listelements_by_string <- function(input_list, search_string) {
+get_list_elements_by_string <- function(input_list, search_string) {
 
+  require(purrr)
   input_list %>%
-    names %>%
-    stringr::str_detect(search_string) %>%
-    purrr::keep(input_list, .)
+    # keep requires logical vector > imap_lgl
+    # .y searches on the name of list element
+    keep( imap_lgl(.,  ~ grepl(search_string, .y )))
 }
