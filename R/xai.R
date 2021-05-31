9eabc9c94a5728024a495df0f678b7826ceb6f22
Chaehan So <chaehan.so@gmail.com>
2021-05-31 20:35:41 +0900

    xai.R: added get_pimp_range_List_models_list(), get_pimp_for_models_list()

--- R/xai.R
+++ R/xai.R
@@ -262,6 +262,113 @@ get_xai_explanations <- function(
     })
 }
 
+################################################################################
+# get permutation feature importance (pimp) for models.list
+################################################################################
+get_pimp_for_models_list <- function(
+  models_list, no_permutations = 50, seed = 171) {
+
+  fi.list.DALEX <- models_list %>%
+
+    imap(function(model_object, model_name) {
+
+      # model_object <- models.list$SVM
+      print(paste("*********", model_object$method))
+
+      training.set <- model_object$trainingData %>%
+        select(.outcome, everything())
+
+      target <- training.set$.outcome
+      print(paste("***target"))
+
+      features <- training.set %>% select(-.outcome)
+
+      DALEX.explainer <- DALEX::explain(
+        model = model_object,
+        data = features,
+        y = target,
+        label = model_name, # .y in imap() is list element name
+        colorize = TRUE
+      )
+      print("*** DALEX.explainer")
+
+      set.seed(seed)
+      system.time(
+        DALEX.permutation.fi <- model_parts(
+          explainer = DALEX.explainer,
+          loss_function = loss_root_mean_square,
+          B = no_permutations,
+          type = "ratio"
+        )
+      ) # 78-84s for rf
+
+      print("*** DALEX.permutation.fi")
+
+      # bar_width = 5.9 items / 12.4 composites
+      ( bar.width <- 20 / log(ncol(features)) )
+
+      DALEX.permutation.fi.plot <- DALEX.permutation.fi %>%
+        plot(
+          bar_width = bar.width
+          , show_boxplots = FALSE
+          , title = "Permutation Feature importance"
+          , subtitle = ""
+        ) +
+        scale_y_continuous(expand = expansion()) # reduce space to axis
+
+      print("*** DALEX.permutation.fi.plot")
+
+      return(
+        list(
+          DALEX.explainer = DALEX.explainer
+          , DALEX.permutation.fi = DALEX.permutation.fi
+          , DALEX.permutation.fi.plot = DALEX.permutation.fi.plot
+        )
+      )
+    })
+  # 44s B=10, 220s B=50, 408s B=100
+
+  return(fi.list.DALEX)
+}
+
+#####################################################
+# calculate permutation feature importance
+#####################################################
+get_pimp_range_List_models_list <- function(
+  list_models_list, model_string = "", value_function = max) {
+
+  list_models_list %>%
+    {
+      if (model_string != "") {
+
+        get_list_elements_by_string(., model_string)
+
+      } else {
+        .
+      }
+    } %>%
+    {
+      # TRICKY: temporary assignment works only for . not names(.) bec it
+      # is piped through, so call models.selected %>% names later
+      {
+        . -> models.selected
+      } %>%
+        imap_dfr(function(models_list, models_list_label) {
+          models_list %>%
+            imap(function(model, model_label) {
+              model$DALEX.permutation.fi %>%
+                # remove the baseline model (seems without any predictor)
+                filter(variable != "_baseline_") %>%
+                group_by(variable) %>%
+                summarise(mean_dropout_loss = mean(dropout_loss)) %>%
+                .$mean_dropout_loss %>%
+                value_function
+            })
+        }) %>%
+        as.data.frame %>%
+        set_rownames(models.selected %>% names)
+    }
+}
 
 ################################################################################
 # List variable importance scores
@@ -339,3 +446,5 @@ visualize_variable_importance_rf <- function(rf_object) {
     scale_y_continuous(expand = c(0,0), limits = c(0,102)) +
     xlab("item") + ylab("variable importance")
 }
+
+
