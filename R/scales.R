a5b4f7613a7b9b151886eb1cee24bab2b3949766
Chaehan So <chaehan.so@gmail.com>
2021-09-03 18:08:43 +0900

    added scales.R

---
+++ R/scales.R
@@ -0,0 +1,80 @@
+
+# FFMQ
+scale.truth.rarely.5 <- c(
+  "Very rarely true",
+  "Rarely true",
+  "Sometimes true",
+  "Often true",
+  "Very often true"
+)
+
+# SDT, virtualcompanion daily
+scale.agreement.little.5 <- c(
+  "Not at all",
+  "A little",
+  "Moderately",
+  "A lot",
+  "Extremely"
+)
+
+# I-PANAS-SF
+scale.agreement.slightly.5 <- c(
+  "Not at all",
+  "Slightly",
+  "Moderately",
+  "Very",
+  "Extremely"
+)
+
+# SLWS
+scale.agreement.slightly.7 <- c(
+  "Strongly disagree",
+  "Disagree",
+  "Slightly disagree",
+  "Neither agree nor disagree",
+  "Slightly agree",
+  "Agree",
+  "Strongly agree"
+)
+
+# UCLA Loneliness
+scale.frequency.4 <- c(
+  "Never",
+  "Rarely",
+  "Sometimes",
+  "Often"
+)
+
+# FFMQ
+scale.frequency.almost.6 <- c(
+  "Almost never",
+  "Very infrequently",
+  "Somewhat infrequently",
+  "Somewhat frequently",
+  "Very frequently",
+  "Almost always"
+)
+
+# SISE
+scale.esteem.semdiff.5 <- c(
+  "1 Not very true of me",
+  "2",
+  "3",
+  "4",
+  "5 Very true of me"
+)
+
+# Fordyce SISE
+scale.happy.11 <- c(
+  "0 Extremely unhappy (utterly depressed, completely down)",
+  "1 Very unhappy (depressed, spirits very low)",
+  "2 Pretty unhappy (somewhat \"blue\", spirits down)",
+  "3 Mildly unhappy (just a bit low)",
+  "4 Slightly unhappy (just a bit below neutral)",
+  "5 Neutral (not particularly happy or unhappy)",
+  "6 Slightly happy (just a bit above neutral)",
+  "7 Mildly happy (feeling fairly good and somewhat cheerful)",
+  "8 Pretty happy (spirits high, feeling good)",
+  "9 Very happy (feeling really good, elated!)",
+  "10 Extremely happy (feeling ecstatic, joyous, fantastic!)"
+)
